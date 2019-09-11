5407fdc458fcd926aa14ba14daef516414124999
Anh <nd.anh@hutech.edu.vn>
2019-09-11 08:48:52 +0700

    Thêm các phím tắt thông dụng trong khi lập tình trên Visual Studio

--- WebsiteBanSach/Controllers/HomeController.cs
+++ WebsiteBanSach/Controllers/HomeController.cs
@@ -8,6 +8,11 @@ namespace WebsiteBanSach.Controllers
 {
     public class HomeController : Controller
     {
+        //Ctrl + K + C: Comment
+        //Ctrl + K + U: Uncomment
+        //Ctrl + K + D: fomrat code
+        //Ctrl + E + W: Wrap code
+        //Ctrl + space: Goi y 
 
         public ActionResult Index()
         {
